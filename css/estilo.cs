63d33bf39e326693ee43c9e1636eb40291e3040a
vinimen <44706024+vinimen@users.noreply.github.com>
2020-04-03 21:52:23 -0300

    Create estilo.cs

---
+++ css/estilo.cs
@@ -0,0 +1,115 @@
+.foto-perfil{
+	height: 100%;
+	width: 20%;
+	max-width: 100%;
+	border-radius:50%
+}
+
+.nav-topo{
+	background-color: #363636;
+}
+.textoBranco{
+	color: white !important;
+}
+.meio p {
+	font-family: 'Titillium Web', sans-serif;
+	font-size: 19pt;
+	color: black !important;
+}
+.corSecundaria{
+	background-color: #B0E0E6;
+}
+p{
+	font-family: Lato, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	color: black !important;
+}
+.titulosBrancos{
+	text-transform: uppercase;
+	font-weight: 700;
+	color: black;
+}
+.contatos{
+	height:300px;
+	padding-top: 15px;
+}
+.sobremim p {
+	 font-family: 'Source Sans Pro', sans-serif;
+	 font-size: 20pt;
+	 font-kerning: auto;
+	 font-weight: 500;
+	 font-variant-position: all;
+}
+.aument-transi:hover {
+	transform: scale(1.02);
+	transition:0.4s all cubic-bezier(0.175, 0.885, 0.32, 1.275);
+	box-shadow: 0px 10px 15px 5px; rgba(0, 0, 0, 0.5);
+}
+.aument-transit:hover:before{
+	opacity: 45;
+}
+.aument-transi img{
+    height: 200px;
+	
+}
+.projetos h2{
+	color: black;
+	margin-bottom: 25px;
+}
+.card-deck p{
+	font-family: 'Titillium Web', sans-serif;
+	font-size: 15pt;
+	font-weight: 700;
+	color: black !important;
+}
+html{
+	scroll-behavior: smooth;
+}
+.contatos{
+	background-color: #B0E0E6;
+	margin-bottom: 0;
+}
+.rodape{
+	padding-bottom: 0.1px;
+	margin-top: 0;
+	background-color:  #363636;
+	text-align: center;
+}
+.rodape p {
+	padding-top: 1px;
+	color: white !important;
+}
+.icones{
+	left:50px;
+	color: black;
+	margin-top:50px;
+	width: 100px;
+	height: 100px;
+	float: left;
+	margin-left:0px;
+	transition: all 0.4s ease-out;
+	border-radius: 50%;
+}
+.icones#facebook, .icones#whatsapp, .icones#linkedin{
+	padding: 20px;
+}
+.icones#facebook:hover{
+	background: #3b5998;
+}
+.icones#linkedin:hover{
+	background: #0e76a8;
+}
+.icones#whatsapp:hover{
+	background:  #00FF00;
+}
+.icones:hover{
+	color: white;
+	cursor: pointer;
+	transform: translateY(-20px);
+	box-shadow: 4px 30px 16px 0px rgba(50,50,50,0.45);
+}
+.teste{
+	position:absolute;
+	left: 50%;
+	margin-top : -40px;
+	margin-left: -110px;
+}	
